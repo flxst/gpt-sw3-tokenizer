6ad0fb53412fc9581e6216e176d1e4ddfe7f7955
Felix Stollenwerk <felix.stollenwerk@ai.se>
2022-06-23 13:30:07 +0200

    refactoring of train.sh

--- train.sh
+++ train.sh
@@ -1,96 +1,124 @@
-# DATASET_FILES=data/test.json
-# DATASET_NAME="0"
-# VOCAB_SIZE=100
+#!/bin/bash
 
-# DATASET_FILES=data/books_sv_epub.jsonl
-# DATASET_NAME="1"
-# VOCAB_SIZE=50000
-
-# DATASET_FILES=data/anforanden.jsonl
-# DATASET_NAME="2"
-# VOCAB_SIZE=30000
-
-# DATASET_FILES="data/anforanden.jsonl data/code.json"
-# DATASET_NAME="2p"
-# VOCAB_SIZE=100000
-
-##################################################################################################
-### WIKI: SINGLE LANGUAGE ##############################################################################
-##################################################################################################
-# DATASET_FILES="data/wiki_da.jsonl"
-# DATASET_NAME="3da"
-# DATASET_FILES="data/wiki_en.jsonl"
-# DATASET_NAME="3en"
-DATASET_FILES="data/wiki_is.jsonl"
-DATASET_NAME="3is"
-# DATASET_FILES="data/wiki_no.jsonl"
-# DATASET_NAME="3no"
-# DATASET_FILES="data/wiki_sv.jsonl"
-# DATASET_NAME="3sv"
+### STANDARD PARAMETERS ##############################
+UNICODE_NORMALIZATION="NFC"
+INDIVIDUAL_DIGITS=1
+ADD_PREFIX_SPACE=1
+ADD_WHITESPACE_TOKENS=24
+MINIMUM_FREQUENCY=0
 VOCAB_SIZE=250000
 ALPHA=-1
+
+
+##################################################################################################
+### TEST DATA (WITH CODE) ########################################################################
+##################################################################################################
+
+# anforanden
+if [ 0 -eq 1 ]
+then
+  ListDatasetFiles=("data/anforanden.jsonl")
+  ListDatasetName=("2")
+  VOCAB_SIZE=100000
+  ADD_PREFIX_SPACE=0
+  ADD_WHITESPACE_TOKENS=0
+fi
+
+# anforanden + code
+if [ 0 -eq 1 ]
+then
+  # ListDatasetFiles=("data/anforanden.jsonl data/code.json data/fibrec.json")
+  ListDatasetFiles=("data/anforanden.jsonl
+                     data/code.json data/code.json data/code.json data/code.json data/code.json data/code.json
+                     data/fibrec.json data/fibrec.json data/fibrec.json data/fibrec.json data/fibrec.json data/fibrec.json")
+  ListDatasetName=("2p")
+  VOCAB_SIZE=100000
+  ADD_PREFIX_SPACE=0
+  ADD_WHITESPACE_TOKENS=0
+fi
+
+##################################################################################################
+### WIKI: SINGLE LANGUAGE ########################################################################
+##################################################################################################
+if [ 0 -eq 1 ]
+then
+  ListDatasetFiles=("data/wiki_da.jsonl"
+                    "data/wiki_en.jsonl"
+                    "data/wiki_is.jsonl"
+                    "data/wiki_no.jsonl"
+                    "data/wiki_sv.jsonl")
+  ListDatasetName=("3da" "3en" "3is" "3no" "3sv")
+fi
 
 ##################################################################################################
 ### WIKI: ALL ##########################################################################################
 ##################################################################################################
-# DATASET_FILES="data/wiki_da.jsonl data/wiki_en.jsonl data/wiki_is.jsonl data/wiki_no.jsonl data/wiki_sv.jsonl"
+
 # ALPHA=1.0
+if [ 0 -eq 1 ]
+then
+  ListDatasetFiles=("data/wiki_da.jsonl
+                     data/wiki_en.jsonl
+                     data/wiki_is.jsonl
+                     data/wiki_no.jsonl
+                     data/wiki_sv.jsonl")
+  ListDatasetName=("3all")
+  ALPHA=1.0
+fi
 
-# DATASET_FILES="data/wiki_da.jsonl data/wiki_da.jsonl
-#  data/wiki_en.jsonl
-#  data/wiki_is.jsonl data/wiki_is.jsonl data/wiki_is.jsonl
-#  data/wiki_no.jsonl data/wiki_no.jsonl
-#  data/wiki_sv.jsonl data/wiki_sv.jsonl"
 # ALPHA=0.8
+if [ 0 -eq 1 ]
+then
+  ListDatasetFiles=("data/wiki_da.jsonl data/wiki_da.jsonl
+                     data/wiki_en.jsonl
+                     data/wiki_is.jsonl data/wiki_is.jsonl data/wiki_is.jsonl
+                     data/wiki_no.jsonl data/wiki_no.jsonl
+                     data/wiki_sv.jsonl data/wiki_sv.jsonl")
+  ListDatasetName=("3all")
+  ALPHA=0.8
+fi
 
-# DATASET_FILES="data/wiki_da.jsonl data/wiki_da.jsonl data/wiki_da.jsonl data/wiki_da.jsonl
-#  data/wiki_en.jsonl
-#  data/wiki_is.jsonl data/wiki_is.jsonl data/wiki_is.jsonl data/wiki_is.jsonl data/wiki_is.jsonl data/wiki_is.jsonl data/wiki_is.jsonl data/wiki_is.jsonl data/wiki_is.jsonl data/wiki_is.jsonl
-#  data/wiki_no.jsonl data/wiki_no.jsonl data/wiki_no.jsonl data/wiki_no.jsonl
-#  data/wiki_sv.jsonl data/wiki_sv.jsonl data/wiki_sv.jsonl"
 # ALPHA=0.6
-
-# DATASET_NAME="3all"
-# VOCAB_SIZE=250000
+if [ 0 -eq 1 ]
+then
+  ListDatasetFiles=("data/wiki_da.jsonl data/wiki_da.jsonl data/wiki_da.jsonl data/wiki_da.jsonl
+                     data/wiki_en.jsonl
+                     data/wiki_is.jsonl data/wiki_is.jsonl data/wiki_is.jsonl data/wiki_is.jsonl data/wiki_is.jsonl data/wiki_is.jsonl data/wiki_is.jsonl data/wiki_is.jsonl data/wiki_is.jsonl data/wiki_is.jsonl
+                     data/wiki_no.jsonl data/wiki_no.jsonl data/wiki_no.jsonl data/wiki_no.jsonl
+                     data/wiki_sv.jsonl data/wiki_sv.jsonl data/wiki_sv.jsonl")
+  ListDatasetName=("3all")
+  ALPHA=0.6
+fi
 
 ##################################################################################################
 ### FINAL100: SINGLE LANGUAGE ####################################################################
 ##################################################################################################
-# DATASET_FILES="data/data_100_final/books/final_da.jsonl
-#  data/data_100_final/conversational/final_da.jsonl
-#  data/data_100_final/misc/final_da.jsonl
-#  data/data_100_final/web_commoncrawl/final_da.jsonl
-#  data/data_100_final/web_sources/final_da.jsonl"
-# DATASET_NAME="4da"
-# DATASET_FILES="data/wiki_en.jsonl"
-# DATASET_NAME="3en"
-# DATASET_FILES="data/wiki_is.jsonl"
-# DATASET_NAME="3is"
-# DATASET_FILES="data/wiki_no.jsonl"
-# DATASET_NAME="3no"
-# DATASET_FILES="data/wiki_sv.jsonl"
-# DATASET_NAME="3sv"
-
-# VOCAB_SIZE=250000
-# ALPHA=-1
+if [ 0 -eq 1 ]
+then
+  ListDatasetFiles=("data/data_100_final/books/final_da.jsonl
+                     data/data_100_final/conversational/final_da.jsonl
+                     data/data_100_final/misc/final_da.jsonl
+                     data/data_100_final/web_commoncrawl/final_da.jsonl
+                     data/data_100_final/web_sources/final_da.jsonl")
+  ListDatasetName=("4da")
+fi
 
 ##################################################################################################
-### ALL+ #########################################################################################
+### RUNS #########################################################################################
 ##################################################################################################
-# DATASET_FILES="data/wiki_da.jsonl data/wiki_en.jsonl data/wiki_is.jsonl data/wiki_no.jsonl data/wiki_sv.jsonl data/code.json"
-# DATASET_NAME="3all+"
-# VOCAB_SIZE=100000
-
-### EXCLUDED OPTIONS ####
-# python script_train.py --dataset_files $DATASET_FILES --dataset_name $DATASET_NAME --add_prefix_space 0 --individual_digits 0 --minimum_frequency 0 --unicode_normalization NFC --vocab_size $VOCAB_SIZE --add_whitespace_tokens 0
-# python script_train.py --dataset_files $DATASET_FILES --dataset_name $DATASET_NAME --add_prefix_space 0 --individual_digits 0 --minimum_frequency 0 --unicode_normalization NFKD --vocab_size $VOCAB_SIZE --add_whitespace_tokens 0
-# python script_train.py --dataset_files $DATASET_FILES --dataset_name $DATASET_NAME --add_prefix_space 0 --individual_digits 0 --minimum_frequency 0 --unicode_normalization NFKC --vocab_size $VOCAB_SIZE --add_whitespace_tokens 0
-# python script_train.py --dataset_files $DATASET_FILES --dataset_name $DATASET_NAME --add_prefix_space 0 --individual_digits 1 --minimum_frequency 0 --unicode_normalization NFC --vocab_size $VOCAB_SIZE --add_whitespace_tokens 0
-# python script_train.py --dataset_files $DATASET_FILES --dataset_name $DATASET_NAME --add_prefix_space 1 --individual_digits 1 --minimum_frequency 0 --unicode_normalization NFC --vocab_size $VOCAB_SIZE --add_whitespace_tokens 0
-
-### TEST OPTIONS ###
-# python script_train.py --dataset_files $DATASET_FILES --dataset_name $DATASET_NAME --add_prefix_space 0 --individual_digits 1 --minimum_frequency 0 --unicode_normalization NFC --vocab_size $VOCAB_SIZE --add_whitespace_tokens 0
-# python script_train.py --dataset_files $DATASET_FILES --dataset_name $DATASET_NAME --add_prefix_space 0 --individual_digits 1 --minimum_frequency 0 --unicode_normalization NFC --vocab_size $VOCAB_SIZE --add_whitespace_tokens 10
-
-### CURRENT OPTIONS ####
-python script_train.py --dataset_files $DATASET_FILES --dataset_name $DATASET_NAME --unicode_normalization NFC --individual_digits 1 --add_prefix_space 1 --add_whitespace_tokens 24 --minimum_frequency 0 --vocab_size $VOCAB_SIZE --alpha $ALPHA
+if [ 1 -eq 1 ]
+then
+  for i in "${!ListDatasetFiles[@]}";
+  do
+      python script_train.py \
+        --dataset_files ${ListDatasetFiles[i]} \
+        --dataset_name ${ListDatasetName[i]} \
+        --unicode_normalization $UNICODE_NORMALIZATION \
+        --individual_digits $INDIVIDUAL_DIGITS \
+        --add_prefix_space $ADD_PREFIX_SPACE \
+        --add_whitespace_tokens $ADD_WHITESPACE_TOKENS \
+        --minimum_frequency $MINIMUM_FREQUENCY \
+        --vocab_size $VOCAB_SIZE \
+        --alpha $ALPHA
+  done
+fi
