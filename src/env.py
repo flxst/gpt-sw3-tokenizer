ccfda9c3f7fd0dc158d98ca10d2c5ae3a3fc7d4c
Felix Stollenwerk <felix.stollenwerk@ai.se>
2022-07-06 11:22:42 +0200

    allow for absolute paths in env.ini

--- src/env.py
+++ src/env.py
@@ -10,14 +10,31 @@ class Env:
 
     def __init__(self,
                  folder="."):
+
         cwd = os.getcwd()
         base_dir = abspath(join(cwd, folder))
         env_file = join(base_dir, "env.ini")
         config.read(env_file)
 
-        self.data_original = join(base_dir, config['main']['data_original'])
-        self.data_sampled = join(base_dir, config['main']['data_sampled'])
-        self.output = join(base_dir, config['main']['output'])
+        if config['main']['data_original'].startswith("/"):
+            for field in ['data_sampled', 'output']:
+                assert config['main'][field].startswith("/"), \
+                    f"ERROR! mixture of absolute and relative paths encountered in Env"
+            relative_paths = False
+        else:
+            for field in ['data_sampled', 'output']:
+                assert not config['main'][field].startswith("/"), \
+                    f"ERROR! mixture of absolute and relative paths encountered in Env"
+            relative_paths = True
+
+        if relative_paths:
+            self.data_original = join(base_dir, config['main']['data_original'])
+            self.data_sampled = join(base_dir, config['main']['data_sampled'])
+            self.output = join(base_dir, config['main']['output'])
+        else:
+            self.data_original = config['main']['data_original']
+            self.data_sampled = config['main']['data_sampled']
+            self.output = config['main']['output']
 
 
 if __name__ == "__main__":
