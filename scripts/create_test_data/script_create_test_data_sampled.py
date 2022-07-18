55e6176e1f2c36272751bcb5e26c5bc1d2eaf910
Felix Stollenwerk <felix.stollenwerk@ai.se>
2022-07-18 12:51:26 +0200

    fix script_create_test_data_sampled.py

--- scripts/create_test_data/script_create_test_data_sampled.py
+++ scripts/create_test_data/script_create_test_data_sampled.py
@@ -40,24 +40,26 @@ def main():
     env = Env()
     os.makedirs(env.data_sampled, exist_ok=True)
 
-    test_data_file = join(env.data_sampled, "test.json")
+    test_data_file = join(env.data_sampled, "test.jsonl")
     with open(test_data_file, "w", encoding="utf-8") as f:
         for i in range(len(TEST_CORPUS)):
-            f.write(json.dumps({"text": TEST_CORPUS[i]}) + "\n")
+            _dict = {"text": TEST_CORPUS[i]}
+            _dict = _add_features(_dict)
+            f.write(json.dumps(_dict) + "\n")
     print(f"> wrote file '{test_data_file}'")
 
     with open("script_train.py", "r") as f:
         code = f.read().replace("\\n", "\n")
     print(code)
 
-    code_data_file = join(env.data_sampled, "code.json")
+    code_data_file = join(env.data_sampled, "code.jsonl")
     _dict = {"text": code}
     _dict = _add_features(_dict)
     with open(code_data_file, "w", encoding="utf-8") as f:
         f.write(json.dumps(_dict))
     print(f"> wrote file '{code_data_file}'")
 
-    fib_rec_file = join(env.data_sampled, "fibrec.json")
+    fib_rec_file = join(env.data_sampled, "fibrec.jsonl")
     fib_rec = "def fibRec(n):\n    if n < 2:\n        return n\n    else:\n        return fibRec(n-1) + fibRec(n-2)"
     _dict = {"text": fib_rec}
     _dict = _add_features(_dict)
